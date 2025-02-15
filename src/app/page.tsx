9c63f31cc414da413d17b70eb03bb5913636d8df
ferhad <137129786+ferhadnaiyev@users.noreply.github.com>
2025-02-15 05:26:13 +0400

    b

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       </div>
       <CompanyLogos />
       <ServicesSection />
-      {/* <CaseStudiesSection /> */}
+      <CaseStudiesSection />
       <OurWorkingProcessSection />
       <TeamSection />
       <TestimonialsSection />
--- src/hooks/useSeparateIntoLines/index.ts
+++ src/hooks/useSeparateIntoLines/index.ts
@@ -1,14 +1,1 @@
-export const separateIntoLines = (text: string, lineCount: number) => {
-    const result: string[] = []
-    const wordCount = text.split(" ").length;
-    const wordCountEachLine = wordCount / lineCount
 
-    for i in range
-
-    // const lineLength = text.length / lineCount;
-    // const spaceIndex = text.indexOf(" ", lineLength);
-
-    // if (spaceIndex === -1) return text;
-
-    // return [text.slice(0, spaceIndex), text.slice(spaceIndex + 1)];
-};
